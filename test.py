14f682c6b87db31589782b9fa79d7adf1ecd0974
Fiblip <filip98andersson@gmail.com>
2023-11-08 20:11:20 +0100

    renaming and restructuring

--- test.py
+++ test.py
@@ -4,27 +4,29 @@ import numpy as npArray
 
 pygame.init()
 
-class tile:
+class tile4Matrix:
     def __init__(self):
-        self.outline = False
         self.visible = True
-        self.posX = None
-        self.posY = None
-        self.posCenterX = None
-        self.posCenterY = None
+        self.data = None
 
-# parameters
-#displayInfo = pygame.display.Info()
-#screenHeight = displayInfo.current_h
-#screenWidth = displayInfo.current_w
-screenScaleFactor = 80
-screenHeight = 9*screenScaleFactor
-screenWidth = 16*screenScaleFactor
-screen = pygame.display.set_mode([screenWidth, screenHeight])
+class tileData:
+    def __init__(self):
+        self.size = None
+        self.spacing = None
+        self.radius = None
+        self.outline = None
 
-run = True
-fps = 30
-timer = pygame.time.Clock()
+class sideTileClass():
+    def __init__(self):
+        self.data = None
+
+class boardClass():
+    def __init__(self):
+        self.height = None
+        self.width = None
+        self.x = None
+        self.y = None
+        self.excess = None
 
 # colors
 white = (255, 255, 255)
@@ -33,24 +35,30 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-# variables
-totalTiles = 8
-maxRatio = 3/2
+# adjustable parameters
+totalTiles = 22
+minRatio = 3/2
 boardScaleFactor = 0.9
 tileScaleFactor = 0.9
-
-# side bar
+radiusScaleFactor = 10
+outlineScaleFactor = 30
 sideBarScaleFactor = 1/3
-sideBarWidth = screenWidth*sideBarScaleFactor
-sideBarHeight = screenHeight
-
-# side bar tiles
 sideTileScaleFactor = 0.65
-sideTileSize = sideBarWidth*sideTileScaleFactor
-sideTile1PosX = (sideBarWidth - sideTileSize)/2
-sideTile1PosY = (sideBarHeight - 2*sideTileSize)/3
-sideTile2PosX = sideTile1PosX
-sideTile2PosY = sideTileSize + 2*(sideBarHeight - 2*sideTileSize)/3
+fps = 60
+
+# screen, side bar and game variables
+displayInfo = pygame.display.Info()
+screenHeight = displayInfo.current_h
+screenWidth = displayInfo.current_w
+#screenScaleFactor = 80
+#screenHeight = 9*screenScaleFactor
+#screenWidth = 16*screenScaleFactor
+screen = pygame.display.set_mode([screenWidth, screenHeight])
+
+sideBar = pygame.Rect([0, 0, screenWidth*sideBarScaleFactor, screenHeight])
+
+timer = pygame.time.Clock()
+run = True
 
 # functions
 def calculateRowsColumns(product):
@@ -65,131 +73,108 @@ def calculateRowsColumns(product):
                 rows = min(i, product/i)
                 columns = max(i, product/i)
         
-        if(columns/rows > maxRatio):
+        if(columns/rows > minRatio):
             rows, columns = calculateRowsColumns(product + 2)
         
     return(int(rows), int(columns))
 
+def calculateSideTiles():
+    sideTileList = [sideTileClass(), sideTileClass()]
+
+    size = sideBar.width*sideTileScaleFactor
+    posX1 = (sideBar.width - size)/2
+    posY1 = (sideBar.height - 2*size)/3
+    posX2 = posX1
+    posY2 = size + 2*(sideBar.height - 2*size)/3
+
+    sideTileList[0].data = pygame.Rect([posX1, posY1, size, size])
+    sideTileList[1].data = pygame.Rect([posX2, posY2, size, size])   
+
+    return(sideTileList)    
+
 def calculateBoard():
-    rows, columns = calculateRowsColumns(totalTiles)
+    board = boardClass()
+    tile = tileData()
 
-    maxBoardHeight = screenHeight - 2*sideTile1PosY
+    maxBoardHeight = screenHeight - 2*sideTileList[0].data.y
 
-    boardWidth = (screenWidth - sideBarWidth)*boardScaleFactor
-    tileSize = boardWidth*tileScaleFactor/columns
-    tileSpacing = (boardWidth - tileSize*columns)/(columns - 1)
+    board.width = (screenWidth - sideBar.width)*boardScaleFactor
+    tile.size = board.width*tileScaleFactor/columns
+    tile.spacing = (board.width - tile.size*columns)/(columns - 1)
 
-    if((tileSize*rows + tileSpacing*(rows - 1)) >= maxBoardHeight):
-        boardHeight = maxBoardHeight
-        tileSize = boardHeight*tileScaleFactor/rows
-        tileSpacing = (boardHeight - tileSize*rows)/(rows - 1)
-        boardWidth = columns*tileSize + tileSpacing*(columns - 1)
+    if((tile.size*rows + tile.spacing*(rows - 1)) >= maxBoardHeight):
+        board.height = maxBoardHeight
+        tile.size = board.height*tileScaleFactor/rows
+        tile.spacing = (board.height - tile.size*rows)/(rows - 1)
+        board.width = columns*tile.size + tile.spacing*(columns - 1)
 
     else:
-        boardHeight = rows*tileSize + tileSpacing*(rows - 1)
+        board.height = rows*tile.size + tile.spacing*(rows - 1)
 
-    boardPosY = (screenHeight - boardHeight)/2
-    boardPosX = sideBarWidth + (screenWidth - (sideBarWidth + boardWidth))/2
-    excess = rows*columns - totalTiles
+    board.y = (screenHeight - board.height)/2
+    board.x = sideBar.width + (screenWidth - (sideBar.width + board.width))/2
+    board.excess = rows*columns - totalTiles
 
-    return(rows, columns, tileSize, tileSpacing, boardHeight, boardWidth, boardPosX, boardPosY, excess)
+    tile.radius = round(tile.size/radiusScaleFactor)
+    tile.outline = round(tile.size/outlineScaleFactor)
 
-def tilePositions():
-    tilesPosX = npArray.empty(columns, dtype=int)
-    tilesPosY = npArray.empty(columns, dtype=int)
-    tilesCenterPosX = npArray.empty(columns, dtype=int)
-    tilesCenterPosY = npArray.empty(columns, dtype=int)
+    return(tile, board)
 
-    for i in range(columns):
-        tilesPosX[i] = boardPosX + i*(tileSize + tileSpacing)
-    
-    for i in range(columns):
-        tilesPosY[i] = boardPosY + i*(tileSize + tileSpacing)
-
-    tilesCenterPosX = tilesPosX + tileSize/2
-    tilesCenterPosY = tilesPosY + tileSize/2
-
-    return(tilesPosX, tilesPosY, tilesCenterPosX, tilesCenterPosY)
-
-def generateBackgrounds():
-    sideBarObject = pygame.draw.rect(screen, black, [0, 0, sideBarWidth, sideBarHeight])
-    tile1Object = pygame.draw.rect(screen, white, [sideTile1PosX, sideTile1PosY, sideTileSize, sideTileSize])
-    tile2Object = pygame.draw.rect(screen, white, [sideTile2PosX, sideTile2PosY, sideTileSize, sideTileSize])
-    #boardObject = pygame.draw.rect(screen, black, [boardPosX, boardPosY, boardWidth, boardHeight])
+def drawElements():
+    pygame.draw.rect(screen, black, sideBar)
+    pygame.draw.rect(screen, white, sideTileList[0].data, 0, 20)
+    pygame.draw.rect(screen, white, sideTileList[1].data, 0, 20)
+    #boardObject = pygame.draw.rect(screen, black, [board.x, board.y, board.width, board.height])
 
 def drawTiles():
     for i in range(rows):
 
         if(i == (rows - 1)):
-            for j in range(columns - excess):
-                pygame.draw.rect(screen, blue, [tilesPosX[j], tilesPosY[i], tileSize, tileSize])
+            for j in range(columns - board.excess):
+                if(tileMatrix[i][j].visible == True):
+                    pygame.draw.rect(screen, blue, tileMatrix[i][j].data, 0, tile.radius)
+
+                    if(tileMatrix[i][j].data.collidepoint(pygame.mouse.get_pos())):
+                        pygame.draw.rect(screen, green, tileMatrix[i][j].data, tile.outline, tile.radius)
 
         else:
             for j in range(columns):
-                pygame.draw.rect(screen, blue, [tilesPosX[j], tilesPosY[i], tileSize, tileSize])
+                if(tileMatrix[i][j].visible == True):
+                    pygame.draw.rect(screen, blue, tileMatrix[i][j].data, 0, tile.radius)
 
-def getPosition(x, y):
-    for i in range(len(tilesPosY)):
-
-        if((y >= tilesPosY[i]) & (y <= (tilesPosY[i] + tileSize))):
-            row = i
-            break
-
-        else:
-            row = None
-
-    for j in range(len(tilesPosX)):
-
-        if((x >= tilesPosX[j]) & (x <= (tilesPosX[j] + tileSize))):
-            column = j
-            break
-
-        else:
-            column = None
-    
-    return(row, column)
+                    if(tileMatrix[i][j].data.collidepoint(pygame.mouse.get_pos())):
+                        pygame.draw.rect(screen, green, tileMatrix[i][j].data, tile.outline, tile.radius)
 
 def generateTileMatrix():
-    tileMatrix = npArray.full((rows, columns), tile())
+    tileMatrix = [[tile4Matrix() for i in range(columns)] for j in range(rows)]
     
-    if(excess != 0):
-        for i in range(excess):
-            tileMatrix[rows - 1, columns - 1 - i] = None
+    if(board.excess != 0):
+        for i in range(board.excess):
+            tileMatrix[rows - 1][columns - 1 - i] = None
+
+    for i in range(rows):
+        if(i == (rows - 1)):
+            for j in range(columns - board.excess):
+
+                tileMatrix[i][j].data = pygame.Rect([board.x + j*(tile.size + tile.spacing), board.y + i*(tile.size + tile.spacing), tile.size, tile.size])
+
+        else:
+            for j in range(columns):
+                tileMatrix[i][j].data = pygame.Rect([board.x + j*(tile.size + tile.spacing), board.y + i*(tile.size + tile.spacing), tile.size, tile.size])
     
     return tileMatrix
 
-def assignTilePositions():
-    for i in range(rows):
-
-        if(i == (rows - 1)):
-            for j in range(columns - excess):
-                tileMatrix[i, j].posX = boardPosX + j*(tileSize + tileSpacing)
-                tileMatrix[i, j].posY = boardPosY + i*(tileSize + tileSpacing)
-
-        else:
-            for j in range(columns):
-                tileMatrix[i, j].posX = boardPosX + j*(tileSize + tileSpacing)
-                tileMatrix[i, j].posY = boardPosY + i*(tileSize + tileSpacing)
-                print(i, j)
-                print(boardPosX + j*(tileSize + tileSpacing))  
-
-# code that runs once   
-rows, columns, tileSize, tileSpacing, boardHeight, boardWidth, boardPosX, boardPosY, excess = calculateBoard()
-tilesPosX, tilesPosY, tilesCenterPosX, tilesCenterPosY = tilePositions()
+# code that runs once
+rows, columns = calculateRowsColumns(totalTiles)
+sideTileList = calculateSideTiles()   
+tile, board = calculateBoard()
 tileMatrix = generateTileMatrix()
-assignTilePositions()
-print(tileMatrix[0,0].posX)
-print(tilesPosX[0])
 
 while run:
-
     timer.tick(fps)
     screen.fill(white)
-    generateBackgrounds()
+    drawElements()
     drawTiles()
-    #kan snygga till om muspositionen inte behövs
-    mouseRow, mouseColumn = getPosition(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-    #print(mouseRow, mouseColumn)
 
     for event in pygame.event.get():
 
@@ -199,11 +184,7 @@ while run:
 
         # checks for button presses
  
-        if event.type == pygame.KEYDOWN:
-
-            # funkar inte just nu
-            #if event.key == pygame.K_q and event.key == pygame.K_LCTRL:
-            #    run = False
+        elif event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_q:
                 run = False
